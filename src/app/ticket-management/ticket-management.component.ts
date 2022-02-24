79216dac4bf19e9a5c3ec971a2b60f6155a60e52
sujesh07 <sujesh@sbsolutionsnepal.com>
2022-02-25 00:02:36 +0545

    Add condition for reserve and payment

--- src/app/ticket-management/ticket-management.component.ts
+++ src/app/ticket-management/ticket-management.component.ts
@@ -11,7 +11,7 @@ import {Ticket} from "../modal/ticket";
 export class TicketManagementComponent implements OnInit {
   displayedColumns: string[] = ['id', 'description', 'price', 'reserved', 'payment'];
 
-  tickets: Array<Ticket> = [{id: 1, description: 'Movie Ticket', price: 250, reserved: false, payment: false},
+  tickets: Array<Ticket> = [{id: 1, description: 'Movie Ticket', price: 250, reserved: true, payment: true},
     {id: 2, description: 'Flight Ticket', price: 375000, reserved: false, payment: false},
     {id: 3, description: 'Heli Ticket', price: 125000, reserved: false, payment: false}];
 
