40060d2461beffeccd32dee31aa774f34f713ad1
sujesh07 <sujesh@sbsolutionsnepal.com>
2022-02-25 04:43:57 +0545

    Refactor

--- src/app/service/httpclient.service.ts
+++ src/app/service/httpclient.service.ts
@@ -7,29 +7,18 @@ export class Employee {
     public name: string,
     public designation: string,
     public salary: string
-  ) {}
+  ) {
+  }
 }
 
 @Injectable({
   providedIn: "root"
 })
 export class HttpClientService {
-  constructor(private httpClient: HttpClient) {}
-
-  getEmployees() {
-    return this.httpClient.get<Employee[]>("http://localhost:8080/employees");
+  constructor(private httpClient: HttpClient) {
   }
 
-  // public deleteEmployee(employee) {
-  //   return this.httpClient.delete<Employee>(
-  //     "http://localhost:8080/employees" + "/" + employee.empId
-  //   );
-  // }
-  //
-  // public createEmployee(employee) {
-  //   return this.httpClient.post<Employee>(
-  //     "http://localhost:8080/employees",
-  //     employee
-  //   );
-  // }
+  getTickets() {
+    return this.httpClient.get<Employee[]>("http://localhost:8080/tickets");
+  }
 }
--- src/app/ticket-management/ticket-management.component.ts
+++ src/app/ticket-management/ticket-management.component.ts
@@ -9,16 +9,28 @@ import {Ticket} from "../modal/ticket";
 })
 
 export class TicketManagementComponent implements OnInit {
-  displayedColumns: string[] = ['id', 'description', 'price', 'reserved', 'payment'];
+  displayedColumns: string[] = ['description', 'price', 'reserved', 'payment'];
 
-  tickets: Array<Ticket> = [{id: 1, description: 'Movie Ticket', price: 250, reserved: true, payment: true},
-    {id: 2, description: 'Flight Ticket', price: 375000, reserved: false, payment: false},
-    {id: 3, description: 'Heli Ticket', price: 125000, reserved: false, payment: false}];
+  // tickets: Array<Ticket> = [{id: 1, description: 'Movie Ticket', price: 250, reserved: true, payment: true},
+  //   {id: 2, description: 'Flight Ticket', price: 375000, reserved: false, payment: false},
+  //   {id: 3, description: 'Heli Ticket', price: 125000, reserved: false, payment: false}];
+
+  tickets: Array<Ticket> = [];
 
   constructor(private httpClientService: HttpClientService) {
   }
 
   ngOnInit() {
+    this.httpClientService
+      .getTickets()
+      .subscribe(response => {
+        console.log('response', response);
+        this.handleSuccessfulResponse(response);
+      });
+  }
+
+  handleSuccessfulResponse(response: any) {
+    this.tickets = response;
   }
 
   addPayTicket(payTicket: Ticket) {
